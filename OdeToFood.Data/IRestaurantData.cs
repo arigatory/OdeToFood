4e5a20cb9e3d3de377fdce38bf6458bc3f03630e
Ivan MSI <arigatory@gmail.com>
2021-11-07 00:15:06 +0300

    build ui

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -12,6 +12,7 @@ namespace OdeToFood.Data
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant updatedRestaurant);
         Restaurant Delete(int id);
+        int GetCountOfRestaurants();
         int Commit();
     }
 }
